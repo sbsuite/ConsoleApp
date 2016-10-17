94ab95a62fa21bb54968a6ed9db6b1ef5b522b25
Robert <rob@robertmcintosh.ca>
2016-10-17 10:49:52 -0400

    adding miktex check as a test

--- dev/TestApp/Program.cs
+++ dev/TestApp/Program.cs
@@ -1,14 +1,55 @@
-﻿namespace TestApp
+﻿using System;
+using Microsoft.Win32;
+
+namespace TestApp
 {
     class Program
     {
+        private const string _mikTEXRegistryPath = @"HKEY_LOCAL_MACHINE\SOFTWARE\BTS Products\MikTEX";
+        public static string MikTEXPortablePath
+        {
+            get
+            {
+                try
+                {
+                    var path = (string)Registry.GetValue(_mikTEXRegistryPath, "InstallDir", null);
+                    if (string.IsNullOrEmpty(path))
+                        throw new MikTexInstallationException();
+                    return path;
+
+                }
+                catch (Exception e)
+                {
+                    throw new MikTexInstallationException(e);
+                }
+            }
+        }
+
         static int Main(string[] args)
         {
             foreach (var arg in args)
             {
-                System.Console.WriteLine(arg);
+                Console.WriteLine($"A command line arg {arg}");
             }
+
+            Console.WriteLine($"MikText is installed: {MikTEXPortablePath}");
+
+
             return 0;
         }
     }
+
+    public class MikTexInstallationException : Exception
+    {
+        public MikTexInstallationException()
+        {
+        }
+
+        public MikTexInstallationException(Exception exception)
+        {
+            
+        }
+    }
+
+
 }
