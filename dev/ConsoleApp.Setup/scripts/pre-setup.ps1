3410123b91ffab4565b12fb095c1998bf6e5ab5c
Michael Sevestre <michael.sevestre@gmail.com>
2016-10-18 09:03:34 -0400

    Add env variable for setup project name

--- dev/ConsoleApp.Setup/scripts/pre-setup.ps1
+++ dev/ConsoleApp.Setup/scripts/pre-setup.ps1
@@ -1,5 +1,5 @@
-$fragments = Get-ChildItem $env:APPVEYOR_BUILD_FOLDER\packages -Filter *.wxs -Recurse | %{$_.FullName}
+$fragments = Get-ChildItem $env:APPVEYOR_BUILD_FOLDER\packages -Filter *.wxs -Recurse | %{$_.FullName} 
 foreach($fragment in $fragments)
 {
-    Copy-Item $fragment  $env:APPVEYOR_BUILD_FOLDER\dev\ConsoleApp.Setup\fragments
+    Copy-Item $fragment  $env:APPVEYOR_BUILD_FOLDER\dev\$env:SETUP_PROJECT_NAME\fragments
 }
