a3628cc67dfebf7d69611b5829f4dc2f29f79d11
Michael Sevestre <michael.sevestre@gmail.com>
2016-10-18 07:35:22 -0400

    First script to deploy wxs

--- dev/ConsoleApp.Setup/scripts/pre-setup.ps1
+++ dev/ConsoleApp.Setup/scripts/pre-setup.ps1
@@ -1,5 +1,5 @@
 $fragments = Get-ChildItem $env:APPVEYOR_BUILD_FOLDER\packages -Filter *.wxs -Recurse | %{$_.FullName}
 foreach($fragment in $fragments)
 {
-    Copy-Item $fragment  C:\dev\Bayer\ConsoleApp\dev\ConsoleApp.Setup\fragments
+    Copy-Item $fragment  $env:APPVEYOR_BUILD_FOLDER\dev\ConsoleApp.Setup\fragments
 }
