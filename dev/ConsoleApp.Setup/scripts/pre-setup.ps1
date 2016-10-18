115cf9cdd215b2461d1771a5a2c72b61b9d864dc
Michael Sevestre <michael.sevestre@gmail.com>
2016-10-18 12:46:55 -0400

    Remove script file

--- dev/ConsoleApp.Setup/scripts/pre-setup.ps1
+++
@@ -1,6 +0,0 @@
-$fragments = Get-ChildItem $env:APPVEYOR_BUILD_FOLDER\packages -Filter *.wxs -Recurse | %{$_.FullName} 
-foreach($fragment in $fragments)
-{
-    Copy-Item $fragment  $env:APPVEYOR_BUILD_FOLDER\dev\$env:SETUP_PROJECT_NAME\fragments
-}
-$env:APP_VERSION
