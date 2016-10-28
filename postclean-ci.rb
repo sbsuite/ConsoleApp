b05be29da0d86c1a63883f6fd8196fda309d006b
Michael Sevestre <michael.sevestre@gmail.com>
2016-10-28 10:44:45 -0400

    Add postclean ci

--- postclean-ci.rb
+++ postclean-ci.rb
@@ -1,15 +1,16 @@
 package_dir = File.join(".", "packages");
 package_rb_files = File.join(package_dir, "**", "*.rb")
-Dir.glob(package_rb_files).each{|x| require_relative x}
+Dir.glob(package_rb_files).each{|x|  require_relative x}
 
 def configuration
-  ENV['CONFIGURATION'] || "Release"
+  ENV['CONFIGURATION'] || "Debug"
 end
 
 puts configuration
 
 target_dir = File.join(".", "src", "ConsoleApp", "bin", configuration);
 
+puts package_dir
 puts target_dir
 
 copy_depdencies package_dir,  target_dir do
