39eedb5028feaae033795eab26e86c491939dc13
Michael Sevestre <michael.sevestre@gmail.com>
2016-10-28 10:26:26 -0400

    Add postclean ci

---
+++ postclean-ci.rb
@@ -0,0 +1,14 @@
+package_dir = File.join(".", "packages");
+package_rb_files = File.join(package_dir, "**", "*.rb")
+Dir.glob(package_rb_files).each{|x| require_relative x}
+
+def configuration
+  ENV['CONFIGURATION'] || "Release"
+end
+
+target_dir = File.join(".", "src", "ConsoleApp", "bin", configuration);
+
+copy_depdencies package_dir,  target_dir do
+  copy_native_dll
+end
+
