a30e43a8f1a6a73e31c9fe98f1e25ab2068862d1
Michael Sevestre <michael.sevestre@gmail.com>
2016-10-31 11:38:50 -0400

    Check what's going on with copying

--- scripts/postclean-ci.rb
+++ scripts/postclean-ci.rb
@@ -5,13 +5,14 @@ def configuration
 end
 
 target_dir = File.join(src_dir, "ConsoleApp", "bin", configuration);
-fragments_dir = File.join(solution_dir, "ConsoleApp.Setup", "fragments");
 test_dir = File.join(solution_dir, "tests", "ConsoleApp.Tests", "bin", configuration);
 
 copy_depdencies packages_dir,  [target_dir, test_dir] do
+	puts "copying to #{target_dir} and #{test_dir}"
   copy_native_dll
 end
 
+fragments_dir = File.join(solution_dir, "ConsoleApp.Setup", "fragments");
 copy_depdencies packages_dir,  fragments_dir do
 	copy_wix_wxs
 end
