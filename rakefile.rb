1585e83ace384751a3d9c37c09641c706eb09ce2
Michael Sevestre <michael.sevestre@gmail.com>
2016-11-01 14:51:39 -0400

    pass version to build script

--- rakefile.rb
+++ rakefile.rb
@@ -1,12 +1,12 @@
 task :default => [:create_setup]
 
-task :create_setup => [:load_dependencies] do
+task :create_setup, [:product_version] => [:load_dependencies] do
 	solution_dir = File.dirname(__FILE__)
 	setup_dir = File.join(solution_dir, 'setup')
-	src_dir = File.join(solution_dir, 'src','ConsoleApp', 'bin', 'Release')
+	src_dir = File.join(solution_dir, 'src', 'ConsoleApp', 'bin', 'Release')
 	deploy_dir = File.join(setup_dir, 'deploy');
 	product_name = 'ConsoleApp'
-	product_version = '1.2.3'
+	product_version = args.product_version
 
 	Rake::Task['setup:all'].execute(OpenStruct.new(
 		:src_dir => src_dir, 
